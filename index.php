d231dc1d8614658d1a53412d69e63452b3294679
noelkerskes666 <56539832+noelkerskes666@users.noreply.github.com>
2020-01-22 16:26:10 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="icon" href="sunny.png">
+    <link href="https://fonts.googleapis.com/css?family=EB+Garamond|Open+Sans&display=swap" rel="stylesheet">
+    <title>Zien & Zijn</title>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+</head>
+<body>
+<div class="content">
+    <img style='height: 100%; width: 100%; object-fit: contain' src='zienzijn.png'/>
+    <p style="text-align:center"> Wilt u een meditatie-training of workshop reserveren? </p>
+    <p style="text-align:center"> Dan bent u op de juiste plek! </p>
+</div>
+<div class="content2">
+    <img style='height: 50%; width: 50%; object-fit: contain' src='poonda2.png'class="panda"/>
+    <button class="button">
+        <a class="button" href="http://localhost:1234/zienzijn/add_afspraak.php" style="">
+            Een afspraak of reserveren maken? Click dan Hier!
+        </a>
+    </button>
+    <img style='height: 50%; width: 50%; object-fit: contain' src='mindfalnoes2.png'class="mind"/>
+</div>
+</div>
+</body>
+</html>
