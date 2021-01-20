52680cd31244b1001eb7a4d944ee0283b18786ba
noelkerskes666 <56539832+noelkerskes666@users.noreply.github.com>
2021-01-20 19:57:32 +0100

    Add files via upload

---
+++ mindfulness.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <link rel="icon" href="sunny.png">
+    <title>Zien & Zijn</title>
+    <link type="text/css" rel="stylesheet" href="style.css"/>
+    <script src="java.js"> </script>
+</head>
+<body>
+    <div class="topnav" id="myTopnav">
+        <a href="index.php" class="active">Home</a>
+        <a href="mindfulness.php" style="background: limegreen" class="active">Mindfulness</a>
+        <a href="yinyoga.php"class="active">Yin yoga</a>
+        <a href="add_afspraak.php"class="active">Reserveren</a>
+        <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+            <i class="fa fa-bars"></i>
+        </a>
+    </div>
+    <div class="content">
+        <h1> Mindfulness </h1>
+        <img src='meditatos1.jpg' class="img2"/>
+    </div>
+    <div class="content3">
+        <h2> <i> Over Minfulness </i> </h2>
+    </div>
+    <div class="flex-container4">
+        <div> <h3> Wat is Mindfulness? </h3>
+            <p> Aandacht is de essentie van mindfulness. Mindful zijn betekent dat we aandacht geven aan onze fysieke en
+                zintuiglijke gewaarwordingen, ons bewust zijn van onze gedachtestroom en onze emotionele sensaties.
+                Door aandacht te geven aan het hier en nu, zonder oordeel en met een vriendelijke, milde houding, laten we toe
+                wat er allemaal is. Doel is de realiteit te herkennen en te accepteren zoals ze is, zonder deze te kleuren door
+                emotie of te reageren vanuit automatisme. Mindfulness is daarmee een levenshouding die ons leert bewuster in
+                het leven te staan en daarmee keuzevrijheid geeft in handelen.</
+            </p>
+        </div>
+        <div> <h3>Hoe doen we dit?</h3>
+            <p> Via aandachtgerichte, meditatie oefeningen: Eenvoudige bewegingsoefeningen, de bodyscan, ademoefeningen en
+                zitmeditatie. Ook is er sprake van informele oefeningen die op elk moment van de dag een plek kunnen
+                krijgen; van het met aandacht luisteren naar de ander tot tanden poetsen. Mediteren betekent: een oefening
+                die helpt om met meer gemak om te kunnen gaan met alle aspecten van het leven; plezierige en onplezierige
+                momenten; momenten die je saai vindt of interessant. Ook word je bewust van de eigen leefstijl. Vaak ook
+                van de wijze waarop je tot nu toe met klachten als pijn, stress, werkdruk of verdriet om bent gegaan.
+            </p>
+        </div>
+        <div> <h3> Hoe ziet de les eruit? </h3>
+            <p> De nadruk van de training ligt op ‘leren door ervaren’ aan de hand van
+                oefeningen, zoals: begeleide aandachtsoefeningen, bewegingsoefeningen, zit -en loopmeditatie. Ook worden er
+                oefeningen gedaan om het bewustzijn in het dagelijkse leven te vergroten. Daarnaast is er ruimte voor het
+                bespreken en delen van de ervaringen. Hierdoor wordt de opmerkzaamheid verscherpt. In de groep onderzoek je
+                je eigen mogelijkheden om bewuster in het leven te staan. Wat wij in onze training bieden is dat we mindfulness
+                vooral praktisch toepasbaar willen maken in het dagelijks leven. Wij kiezen bewust voor een ervaringsgerichte
+                aanpak, waarbij de zintuigen en het lichaam een belangrijke rol spelen.
+            </p>
+        </div>
+    </div>
+    </div>
+    <div class="content3">
+        <h2> <i> Over deze training </i> </h2>
+    </div>
+    <div class="flex-container">
+        <div> <h3> Datum & tijden </h3>
+            <p> Helaas kiezen we mede door de Corona-crisis ervoor om geen groepsaanbod te doen.
+                Heb jij interesse voor een individueel aanbod op maat, laat het ons weten
+            </p>
+        </div>
+        <div>
+            <h3> Kosten & locatie </h3>
+             <p> SiSu Centrum
+                 Meerwedesingel 48
+                 2993 TG Barendrecht
+                 (of gebouw Yulius, Boerhaavelaan 2, 2992 KZ Barendrecht)
+                 € 365,00 (particulieren)
+                 € 495,00 (via werkgever)
+             </p>
+        </div>
+        <div> <h3> Aanmelden </h3>
+            <p> Meld je van te voren aan (liefst 5 dagen),
+                door een mailtje te sturen naar
+                info@mindfulness-barendrecht.nl of stuur een
+                berichtje naar Monica (06-11073140).
+            </p>
+        </div>
+    </div>
+    <div class="content3">
+        <h2> <i> Direct Contact </i> </h2>
+    </div>
+    <div style="border-left: 1px solid black;" class="flex-container3">
+        <div>
+            <img src="buddha1.jpg" class="img1" />
+        </div>
+        <div>
+            <p2> <b> Naam: </b>Monica Kerskes </p2>
+            <br>
+            <p2> <b> Telefoon:</b>06-11073140 </p2>
+            <br>
+            <p2> <b> Mail:</b>info@mindfulness-barendrecht.nl </p2>
+            <p2> <b> locatie:</b>Meerwedesingel 48, Barendrecht </p2>
+            <br>
+            <p> Heb je vragen, opmerkingen of wil je graag meer informatie over Zien en Zijn Mindfulness? Neem gerust contact met ons op. </p>
+        </div>
+    </div>
+    <script type="text/javascript" src="java.js"> </script>
+    <footer>
+        <div class="footer3">
+            <p1> ZIEN & ZIJN MINDFULNESS </p1>
+            <p1> locatie:  Praktijk Centrum Carnisse </p1>
+            <p1> Middeldijkerplein 3A, Barendrecht </p1>
+            <p1> E-mail: info@mindfulness-barendrecht.nl </p1>
+            <p1> Tel: 06-11073140 (Monica) </p1>
+            <p1> KvK: 53674480, Vestigingsnummer: 000023538023 </p1>
+            <p1> BTWnr: 850971287B01 </p1>
+            <p1> Bank: NL10 INGB 0758 0216 66 </p1>
+        </div>
+        <div class="footer2">
+            <p1> Designed door Noël Kerskes | Copyright 2015-2021 Zien en Zijn | Alle rechten voorbehouden | Privacyverklaring </p1>
+        </div>
+    </footer>
+</body>
+</html>
