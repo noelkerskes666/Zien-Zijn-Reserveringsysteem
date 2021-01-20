64081cdd4f4d127622402542bbdad7a6733d1a33
noelkerskes666 <56539832+noelkerskes666@users.noreply.github.com>
2021-01-20 17:49:46 +0100

    Delete mysqli_connect.php

--- mysqli_connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-    DEFINE ('DB_USER', 'afspraakweb');
-    DEFINE ('DB_PASSWORD', 'kerskes666');
-    DEFINE ('DB_HOST', 'localhost');
-    DEFINE ('DB_NAME', 'zienzijn');
-
-    $dbc = @mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME)
-    OR die('Could not connect to MySQL: ' . mysqli_connect_error());
-?>
